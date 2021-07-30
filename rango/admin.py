e1a3a5e0fac87d273f6593a1231ae5968fc6a31c
Jingwen Feng <2570957F@student.gla.ac.uk>
2021-07-30 10:38:13 +0800

    created categoryhtml

--- rango/admin.py
+++ rango/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from rango.models import Category, Page
 
-admin.site.register(Category)
-admin.site.register(Page)
+# Add in this class to customise the Admin Interface 
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('name',)}
+# Update the registration to include this customised interface
+admin.site.register(Category, CategoryAdmin)

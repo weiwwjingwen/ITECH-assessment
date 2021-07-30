83636262f522d1ec83165fd7dcfc5f2df9401681
Jingwen Feng <2570957F@student.gla.ac.uk>
2021-07-30 11:51:38 +0800

    fixed some errors in views

--- rango/forms.py
+++ rango/forms.py
@@ -20,6 +20,14 @@ class PageForm(forms.ModelForm):
 
                         help_text="Please enter the URL of the page.")
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+    def clean(self):
+        cleaned_data = self.cleaned_data 
+        url = cleaned_data.get('url')
+# If url is not empty and doesn't start with 'http://', # then prepend 'http://'.
+        if url and not url.startswith('http://'):
+            url = f'http://{url}'
+            cleaned_data['url'] = url
+        return cleaned_data
 
     class Meta:
 # Provide an association between the ModelForm and a model 

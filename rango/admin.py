9cfe21b2c2e48e653db968dc4e3eff6d728fccc5
Jingwen Feng <2570957F@student.gla.ac.uk>
2021-07-30 14:55:21 +0800

    add Userform

--- rango/admin.py
+++ rango/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from rango.models import Category, Page
+from rango.models import UserProfile
 
 # Add in this class to customise the Admin Interface 
 class CategoryAdmin(admin.ModelAdmin):

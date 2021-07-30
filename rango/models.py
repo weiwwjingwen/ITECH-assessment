bd3299f8eb84d720818d5dff55e872413a3aa97c
Jingwen Feng <2570957F@student.gla.ac.uk>
2021-07-30 10:21:48 +0800

    edited indexhtml

--- rango/models.py
+++ rango/models.py
@@ -5,6 +5,9 @@ class Category(models.Model):
     views = models.IntegerField(default=0)
     likes = models.IntegerField(default=0)
 
+    class Meta:
+        verbose_name_plural = 'Categories'
+
     def __str__(self): 
         return self.name
 
